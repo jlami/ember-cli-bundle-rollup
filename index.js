e6a8cf2112aa856c1e13df5ef2eb6d6c33de572e
Joren l'Ami <joren@distinct.nl>
2016-12-11 21:57:15 +0100

    other packages in root, not subdir of app, to fix import of 'router', which should be separate package, not app/router.js router + route-recognizer as new packages

--- index.js
+++ index.js
@@ -30,10 +30,11 @@ EmberApp.prototype.javascript = function() {
 	
 	var packages = stew.find([stew.find('node_modules/glimmer-engine/dist/es6/', { desDir: ''}), stew.rename('node_modules/ember-source/packages/', '/lib/', '/')]);
 	
-	tree = stew.find([tree, stew.mv(packages, 'test-es6/')]);
-	let tree2 = stew.find([__dirname + '/lib'], { destDir: 'test-es6'});
+//	tree = stew.find([tree, stew.mv(packages, 'vendor/modules/')]);
+	tree = stew.find([tree, packages]);//no overwrite, because modules should not have conflicting names
+	let tree2 = stew.find([__dirname + '/lib'], { destDir: '/'});
 		
-	tree = stew.mv(tree, 'vendor/modules/', 'test-es6/');
+	tree = stew.mv(tree, 'vendor/modules/', '/');
 	tree = stew.find([tree, tree2], { overwrite: true });
 
 	let baseurl = null;
@@ -43,6 +44,8 @@ EmberApp.prototype.javascript = function() {
 	  });
 	tree = stew.debug(tree, 'rollup4');
 	//return tree;
+	
+	let root;
   
 	return new Rollup(tree, {
 		rollup: {
@@ -68,20 +71,22 @@ EmberApp.prototype.javascript = function() {
 					options: function(options) {
 						//console.log(options.entry);
 						baseurl = path.dirname(options.entry);
+						root = path.resolve(baseurl, '..');
 						options.banner = 'window.EmberENV = {"FEATURES":{}};' + fs.readFileSync(path.resolve(baseurl, '../vendor/loader/loader.js'));
-						options.footer = 'testes6.create({"name":"test-es6","version":"0.0.0+2f11d37b"});';
+						options.banner += fs.readFileSync(require.resolve('jquery'));
+						options.footer = 'var testes6app = testes6.create({"name":"test-es6","version":"0.0.0+2f11d37b"});';
 						return options;
 					},
 					resolveId: function(id, loader) {
-						//if (id == 'fs' || id == 'path') console.log(id, loader);
+						if (id == 'fs' || id == 'path') console.log(id, loader);
 						//if (id == 'backburner') return 'backburner.js';
 						
-						if (id == 'ember/version' || id == './version' && path.dirname(path.relative(baseurl, loader)) === 'ember') {
-							return path.resolve(baseurl, 'version.js');
-						}
+//						if (id == 'ember/version' || id == './version' && path.dirname(path.relative(baseurl, loader)) === 'ember') {
+//							return path.resolve(baseurl, 'version.js');
+//						}
 						
 						var mapping = {
-							'ember/features' : 'features.js',
+//							'ember/features' : 'features.js',
 							'./config/environment': 'config.js',
 //							'ember': '../bower_components/ember/ember.js',
 							//'ember': '../vendor/ember-cli-bundle-rollup/ember.js',
@@ -93,16 +98,20 @@ EmberApp.prototype.javascript = function() {
 //							console.log(fs.existsSync(path.resolve(loader, '..')));
 //							console.log(fs.existsSync(path.resolve(loader, '../../')));
 							//console.log(fs.existsSync(path.resolve(loader, '../../vendor')));
-							return path.resolve(baseurl, '.', mapping[id]);
+							return path.resolve(root, mapping[id]);
 						} else if (loader) {
 							let baseFile;
 							if (id[0] !== '.') {
-								baseFile = path.resolve(baseurl, id)
+								baseFile = path.resolve(root, id)
 							} else {
 								baseFile = path.resolve(loader, '..', id);
 							}
 							
 							if (fs.existsSync(baseFile + '.js')) {
+//								if (baseFile === path.resolve(baseurl, 'router')) {
+//									console.log('router from ' + loader);
+//								}
+									
 								//console.log('found file ' + id);
 								return baseFile + '.js';
 							} else {
@@ -130,7 +139,7 @@ EmberApp.prototype.javascript = function() {
 					skip: true,
 				}),
 				commonjs(),
-				require('rollup-plugin-uglify')({}, require('uglify-js').minify),
+//				require('rollup-plugin-uglify')({}, require('uglify-js').minify),
 			],
 			targets: [
 //				{
